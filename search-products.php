6a8cf12bf3a5fc412b34ebf88f04e90d6697c3dc
Runal <runalb@users.noreply.github.com>
2021-06-10 17:50:36 +0530

    BE - Renamed - pages: add, search, view available products

--- search-products.php
+++ search-products.php
@@ -7,21 +7,20 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Products</title>
+    <title>Search Products</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css" >
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="vender/css/all.css">
 </head>
-<body>
+<body class="bg-search">
 <?php
         if($_SESSION["username"] === ""){
           echo $_SESSION['username'];
           echo "login";
           header("Location: index.php ");
-        }
-      ?>
+        }?>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <a class="navbar-brand my-0 mr-md-auto" href="dashboard.php">
             <img src="images/logo.svg" alt="logo" width="130" height="30" alt="Logo" loading="lazy">
@@ -33,29 +32,27 @@ session_start();
       <div class="container h-100">
         <div class="row align-items-center h-100" >
             
-            
           <div class="col-8 mx-auto">
 
-                <div class="shadow-lg bg-white mt-4">
-                    <div class="col form-header text-center p-3">
-                        Add Products
+                <div class="mt-4">
+                    <div class="text-center p-3">
+                        <h3 class="theme-color">Search</h3>
                     </div>
-                    <form action="add-books-action.php" method="get">
-                        <div class="form-group mx-4 mt-4">
-                            <input type="text" required name="productid" class="form-control" placeholder="Product ID">
-                        </div>
-                        <div class="form-group mx-4 mt-4">
-                            <input type="text" required name="productname" class="form-control" placeholder="Product Name">
-                        </div>
-                        <div class="form-group mx-4 mt-4">
-                            <input type="text" required name="productprice" class="form-control" placeholder="Product Price">
-                        </div>
-                        <div class="form-group mx-4 mt-4">
-                            <input type="number" required name="qty" class="form-control" placeholder="Quantity">
-                        </div>
-                        <button type="submit" class="btn login-btn btn-block my-4">Add Product</button>
-                    </form>
                 </div>
+
+                
+                  <form action="search-products-action.php" method="get">
+                  <div class="input-group md-form form-md form-2 pl-0">
+                    <input class="form-control my-0 py-1" name="query" type="text" placeholder="Enter Product ID or Name" aria-label="Search">
+                    <div class="input-group-append">
+                      <button class="btn">
+                      <span class="input-group-text search-btn" id="basic-text1"><i class="fas fa-search search-icon"
+                          aria-hidden="true"></i></span>
+                      </button>
+                    </div>
+                    </div>
+                  </form>
+                  
                 
           </div>
         </div>
